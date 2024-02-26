b5de48ab3ce9fe036ba686bdeb0e71b263b5192e
santiago <asantiago2reyes@gmail.com>
2024-02-26 09:48:53 -0500

    upd

--- interactividad_2.js
+++ interactividad_2.js
@@ -8,11 +8,15 @@ const diccionario = JSON.parse(diccionarioJSON);
 // Imprimir el diccionario en la página
 const diccionarioContainer = document.getElementById('diccionarioContainer');
 for (let clave in diccionario) {
-    console.log("papapapa")
+
     const valor = diccionario[clave];
     const parrafo = document.createElement('p');
-    parrafo.textContent = `${clave}: ${valor}`;
+    parrafo.textContent = `Cliente ${clave}: Sector ${valor[0]} Nombre ${valor[1]} ${valor[2]} ${valor[3]} ${valor[4]} ${valor.slice(5, valor.length -1)} ${valor[valor.length-1]}`;
+    
+    parrafo.style.marginBottom = "10px";
+    const espacio = document.createElement('p');
+    espacio.textContent = ' '; // Puedes ajustar este texto según tus preferencias
     diccionarioContainer.appendChild(parrafo);
-    console.log(valor)
+    console.log(valor[0])
 }
 
